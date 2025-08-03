a118bdeb57fb4b5f539140c60ac8268eff6f7594
Rachuu1200 <rachanamandal100@gmail.com>
2025-08-03 13:24:38 +0545

    Add files via upload

---
+++ for_of_loops.js
@@ -0,0 +1,10 @@
+//for of loops
+
+let fruits = ["Apple","Banana","Otange","Watermelon"]
+for(let fruit of fruits){
+    console.log(fruit)
+}
+// for in loops
+for(let fruit in fruits){
+    console.log(fruits[fruit])
+}
